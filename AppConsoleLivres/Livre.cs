16af881d5753fd4e0e00616c0fe0e0f460128c94
achille loic <300134022@monboreal.ca>
2024-10-20 22:38:21 +0200

    creation et ajout de la classe livre

---
+++ AppConsoleLivres/Livre.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Livre
+    {
+        //atrribut de la classe livre
+        public string Titre {  get; set; }
+        public string Auteur { get; set; }
+        public int NombrePages { get; set; }
+
+        //constructeur 
+        public Livre(string titre,string auteur, int nombrePages)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            NombrePages = nombrePages;
+
+        }
+        //Afficher les details de la classse livre 
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Tire: {Titre}, Auteur: {Auteur}, NombrePages:{NombrePages} ");
+        }
+    }
+}
